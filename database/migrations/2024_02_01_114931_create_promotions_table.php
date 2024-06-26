317d4456a191ff02a8c067ef45bd940e87b04642
steeven_js <jacques.steeven@gmail.com>
2024-06-26 18:56:20 -0400

    chore: Update migrations for pot_types, promotions, colors, plant_types, and promocodes tables

--- database/migrations/2024_02_01_114931_create_promotions_table.php
+++ database/migrations/2024_02_01_114931_create_promotions_table.php
@@ -4,23 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-  /**
-   * Run the migrations.
-   */
-  public function up(): void {
-    Schema::create('promotions', function (Blueprint $table) {
-      $table->id();
-      $table->timestamps();
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
 
-      $table->string('name');
-    });
-  }
+            $table->string('name');
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void {
-    Schema::dropIfExists('promotions');
-  }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
 };
--- database/migrations/2024_02_04_105641_create_reviews_table.php
+++ database/migrations/2024_02_04_105641_create_reviews_table.php
@@ -4,27 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-  /**
-   * Run the migrations.
-   */
-  public function up(): void {
-    Schema::create('reviews', function (Blueprint $table) {
-      $table->id();
-      $table->timestamps();
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
 
-      $table->string('name');
-      $table->string('plant');
-      $table->string('comment');
-      $table->integer('rating');
-      $table->integer('plant_id');
-    });
-  }
+            $table->string('name');
+            $table->string('plant');
+            $table->string('comment');
+            $table->integer('rating');
+            $table->integer('plant_id');
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void {
-    Schema::dropIfExists('reviews');
-  }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
 };
