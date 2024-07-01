4c0d073dc0bd1efbb51fcd3b8f6b1a855599a3f7
steeven_js <jacques.steeven@gmail.com>
2024-06-30 21:14:41 -0400

    chore: Update scheduling in Kernel.php to check subscriptions every six hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('subscriptions:check')->everyTwoMinutes();
+        $schedule->command('subscriptions:check')->everySixHours();
     }
 
     /**
