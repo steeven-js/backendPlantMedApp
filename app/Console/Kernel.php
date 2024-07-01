2277e7d9e027c28e492f9e997c13d5ec964a0f66
steeven_js <jacques.steeven@gmail.com>
2024-06-30 22:09:47 -0400

    chore: Update scheduling in Kernel.php to check subscriptions every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,12 +8,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        Commands\CheckSubscriptions::class,
+    ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('subscriptions:check')->everySixHours();
+        $schedule->command('subscriptions:check')->everyMinute();
         Log::channel('subscription_check')->info('Subscription check completed.');
     }
 
